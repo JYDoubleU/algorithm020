d9afa2dde2bf4dbaa25ca278fb227cc355ec7f91
JYDoubleU <oeohomos@hotmail.com>
2020-11-30 05:46:25 +0800

    week02

---
+++ Week2/inorder_traversal.rs
@@ -0,0 +1,37 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    fn traver(root: &Option<Rc<RefCell<TreeNode>>>, visited: &mut Vec<i32>) {
+        if let Some(root) = root {
+            let root = root.borrow();
+            traver(&root.left, visited);
+            visited.push(root.val);
+            traver(&root.right, visited);
+        }
+    }
+
+    let mut ret = vec![];
+    if let Some(_) = root {
+        traver(&root, &mut ret);
+    }
+    ret
+}
